34393ef736344f59225bf47562f873bf51675ec6
FULLBL00M <alexxsawyer@gmail.com>
2022-12-22 01:45:32 -0500

    Going to sleep. Got a good plan in the works though.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import os
-import sys
 import pprint
 import modules.termtek as ttk
+from typing import List, Callable, Dict, Any
+import sys
+sys.path.append('/path/to/project/modules')
+import modules.termtek.consolemenu as cm
+
 
 def pp(arr):
     pprint.pprint(arr)
@@ -17,49 +20,115 @@ def settings_testone():
 def settings_testtwo():
     print(ttk.build_fancy_font("Settings test two"))
 
+console_menus = [{
+    "menu_name": "root_menu",
+    "banner": "Main Menu",
+    "menu_options": [
+                [ "test1", "Test function one", testone ],
+                [ "test2", "Test funtion two", testtwo ],
+                [ "settings", "Change settings for the program", "settings_menu" ],
+                [ "exit", "Exit the program", sys.exit ]
+    ],
+    "previous_menu": None,
+    "children": [
+        {
+            "menu_name": "openai_api_menu",
+            "banner": "Openai Api Menu",
+            "menu_options": [
+                        [ "test1", "Test openai function one", testone ],
+                        [ "test2", "Test openai funtion two", testtwo ],
+                        [ "settings", "Change settings for openai", "openai_api_settings_menu" ],
+                        [ "back", "Go back to main menu", "root_menu" ]
+            ],
+            "previous_menu": "root_menu",
+            "children": [
+                {
+                    "menu_name": "openai_api_settings_menu",
+                    "banner": "Openai Api Settings Menu",
+                    "menu_options": [
+                                [ "test1", "Test openai settings function one", settings_testone ],
+                                [ "test2", "Test openai settings funtion two", settings_testtwo ],
+                                [ "back", "Go back to main menu", "openai_api_menu" ]
+                    ],
+                    "previous_menu": "openai_api_menu",
+                    "children": []
+                }
+            ]
+        },
+        {
+            "menu_name": "settings_menu",
+            "banner": "Settings Menu",
+            "menu_options": [
+                        [ "password", "Change your account password", settings_testone ],
+                        [ "email", "Change your account email", settings_testtwo ],
+                        [ "back", "Go back to main menu", "root_menu" ]
+            ],
+            "previous_menu": "root_menu",
+            "children": []
+        }
+    ]
+}]
+
 # _________________________________________________________________________________________________________________________________
 
-def create_main_menu(settings_root=None):
-    main_banner = ttk.build_fancy_font("[ Main Menu ]")
-    main_menu_options = [
-        ttk.ConsoleMenuOption("test1", "Test function one", testone),
-        ttk.ConsoleMenuOption("test2", "Test funtion two", testtwo),
-        ttk.ConsoleMenuOption("settings", "Change settings for the program", settings_root),
-        ttk.ConsoleMenuOption("exit", "Exit the program", sys.exit)
-    ]
-    return ttk.ConsoleMenu(main_banner, main_menu_options)
+# def create_main_menu(settings_root=None):
+#     main_banner = ttk.build_fancy_font("[ Main Menu ]")
+#     main_menu_options = [
+#         ttk.ConsoleMenuOption("test1", "Test function one", testone),
+#         ttk.ConsoleMenuOption("test2", "Test funtion two", testtwo),
+#         ttk.ConsoleMenuOption("settings", "Change settings for the program", settings_root),
+#         ttk.ConsoleMenuOption("exit", "Exit the program", sys.exit)
+#     ]
+#     return ttk.ConsoleMenu(main_banner, main_menu_options)
 
-def create_settings_menu(main_menu_root):
-    settings_banner = ttk.build_fancy_font("[ Settings Menu ]")
-    settings_menu_options = [
-        ttk.ConsoleMenuOption("password", "Change your account password", settings_testone),
-        ttk.ConsoleMenuOption("email", "Change your account email", settings_testtwo),
-        ttk.ConsoleMenuOption("back", "Go back to main menu", main_menu_root)
-    ]
-    return ttk.ConsoleMenu(settings_banner, settings_menu_options, main_menu_root)
+# def create_settings_menu(main_menu_root):
+#     settings_banner = ttk.build_fancy_font("[ Settings Menu ]")
+#     settings_menu_options = [
+#         ttk.ConsoleMenuOption("password", "Change your account password", settings_testone),
+#         ttk.ConsoleMenuOption("email", "Change your account email", settings_testtwo),
+#         ttk.ConsoleMenuOption("back", "Go back to main menu", main_menu_root)
+#     ]
+#     return ttk.ConsoleMenu(settings_banner, settings_menu_options, main_menu_root)
 
-def create_all_menus():
-    menu_objects = []
-    main_menu_root = create_main_menu()
-    settings_root = create_settings_menu(main_menu_root)
+# def create_all_menus():
+#     menu_objects = []
+#     main_menu_root = create_main_menu()
+#     settings_root = create_settings_menu(main_menu_root)
 
-    menu_objects.append({
-        "main_menu_object": main_menu_root
-    })
-    menu_objects.append({
-        "settings_menu_object": settings_root
-    })
+#     menu_objects.append({
+#         "main_menu_object": main_menu_root
+#     })
+#     menu_objects.append({
+#         "settings_menu_object": settings_root
+#     })
 
-    for option in menu_objects[0].get("main_menu_object").menu_options:
-        if option.label == "Change settings for the program":
-            option.func = settings_root.show_full
-    return menu_objects
+#     return menu_objects
 
 # _________________________________________________________________________________________________________________________________
 
-def show_main_menu(menu_objects):
-    menu_objects[0].get("main_menu_object").show_full()
+# def show_main_menu(menu_objects):
+#     menu_objects[0].get("main_menu_object").show_full()
 
 if __name__ == "__main__":
-    menu_objects = create_all_menus()
-    show_main_menu(menu_objects)
+
+    def build_menus(menus: List[dict]) -> 'ConsoleMenu':
+        def build_menu(menu: dict) -> 'consoleMenu':
+            menu_options = []
+            for option in menu['menu_options']:
+                key, label, func = option
+                menu_option = cm.ConsoleMenu.ConsoleMenuOption(key, label, func)
+                menu_options.append(menu_option)
+
+            current_menu = cm.ConsoleMenu(menu['banner'], menu_options, menu['previous_menu'])
+            for child in menu['children']:
+                child_menu = build_menu(child)
+                child_menu.set_previous_menu(current_menu)
+                current_menu.menu_options.append(cm.ConsoleMenu.ConsoleMenuOption(child['menu_name'], child['banner'], child_menu.show_full))
+            return current_menu
+
+        root_menu = build_menu(menus[0])
+        return root_menu
+
+    # Example usage
+    root_menu = build_menus(console_menus)
+    root_menu.show_full()
--- modules/termtek/consolemenu.py
+++ modules/termtek/consolemenu.py
@@ -1,63 +1,155 @@
 import os
 
-class ConsoleMenuOption:
-  def __init__(self, key, label, func):
-    self.key = key
-    self.label = label
-    self.func = func
-  
-  def get_key(self):
-    return self.key
-  
-  def get_label(self):
-    return self.label
-  
-  def get_func(self):
-    return self.func
-  
-
 class ConsoleMenu:
+  class ConsoleMenuOption:
+    def __init__(self, key, label, func):
+      self.key = key
+      self.label = label
+      self.func = func
+
+    def get_key(self):
+      return self.key
+
+    def set_key(self, key):
+      self.key = key
+
+    def get_label(self):
+      return self.label
+
+    def set_label(self, label):
+      self.label = label
+
+    def get_func(self):
+      return self.func
+
+    def set_func(self, func):
+      self.func = func
+
   def __init__(self, banner, menu_options=None, previous_menu=None):
     self.banner = banner
+    self.menu_options = menu_options or []
+    self.previous_menu = previous_menu
+  
+  def go_back(self):
+    self.previous_menu.show_full()
+
+  def get_banner(self):
+    return self.banner
+
+  def set_banner(self, banner):
+    self.banner = banner
+
+  def get_menu_options(self):
+    return self.menu_options
+
+  def set_menu_options(self, menu_options):
     self.menu_options = menu_options
+
+  def get_previous_menu(self):
+    return self.previous_menu
+
+  def set_previous_menu(self, previous_menu):
     self.previous_menu = previous_menu
 
   def show_banner(self):
     print(f"\n{self.banner}\n")
 
   def show_menu_options(self):
-    for option in self.menu_options:
-      print(f"[ {option.key} ]: {option.label}")
-    print()
+    if self.menu_options is not None:
+      for option in self.menu_options:
+        print(f"[ {option.key} ]: {option.label}")
+      print()
 
   def show_previous_menu(self):
     if self.previous_menu is not None:
       print(self.previous_menu)
-    else:
-      print("NONE~!")
 
   def show_full(self):
-      """
-      Shows the full menu and returns the user's selection.
-      """
-      while True:
-          print(f"\n{self.banner}\n")
-          for option in self.menu_options:
-              print(f"[ {option.key} ]: {option.label}")
-          print()
-          choice = input("Enter your choice: ")
+    """
+    Shows the full menu and returns the user's selection.
+    """
+    while True:
+        print(f"\n{self.banner}\n")
+        for option in self.menu_options:
+            print(f"[ {option.key} ]: {option.label}")
+        print()
+        choice = input("Enter your choice: ")
 
-          # find the menu option with the matching key
-          selected_option = next((x for x in self.menu_options if x.key == choice), None)
+        # find the menu option with the matching key
+        selected_option = next((x for x in self.menu_options if x.key == choice), None)
 
-          # if a matching option was found, perform the corresponding action
-          if selected_option is not None:
-              print()
-              selected_option.func()
-          # if no matching option was found, exit the main loop
-          else:
-              print("Invalid selection. Please try again.")
-              self.show_full()
+        # if a matching option was found, perform the corresponding action
+        if selected_option is not None:
+            print()
+            selected_option.func()
+        # if no matching option was found, exit the main loop
+        else:
+            print("Invalid selection. Please try again.")
+            self.show_full()
+   
+
+
+
+#_____________________________________________________________________________________________________________________________
+
+# class ConsoleMenuOption:
+#   def __init__(self, key, label, func):
+#     self.key = key
+#     self.label = label
+#     self.func = func
+  
+#   def get_key(self):
+#     return self.key
+  
+#   def get_label(self):
+#     return self.label
+  
+#   def get_func(self):
+#     return self.func
+  
+
+# class ConsoleMenu:
+#   def __init__(self, banner, menu_options=None, previous_menu=None):
+#     self.banner = banner
+#     self.menu_options = menu_options
+#     self.previous_menu = previous_menu
+
+#   def show_banner(self):
+#     print(f"\n{self.banner}\n")
+
+#   def show_menu_options(self):
+#     for option in self.menu_options:
+#       print(f"[ {option.key} ]: {option.label}")
+#     print()
+
+#   def show_previous_menu(self):
+#     if self.previous_menu is not None:
+#       print(self.previous_menu)
+#     else:
+#       print("NONE~!")
+
+#   def show_full(self):
+#       """
+#       Shows the full menu and returns the user's selection.
+#       """
+#       while True:
+#           print(f"\n{self.banner}\n")
+#           for option in self.menu_options:
+#               print(f"[ {option.key} ]: {option.label}")
+#           print()
+#           choice = input("Enter your choice: ")
+
+#           # find the menu option with the matching key
+#           selected_option = next((x for x in self.menu_options if x.key == choice), None)
+
+#           # if a matching option was found, perform the corresponding action
+#           if selected_option is not None:
+#               print()
+#               selected_option.func()
+#           # if no matching option was found, exit the main loop
+#           else:
+#               print("Invalid selection. Please try again.")
+#               self.show_full()
 
 """
 ************************************************************************************************************************************
